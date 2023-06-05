d7d7eaa9a84a466ad5a2a6b7bb6bee19566dece6
lyazj <seeson@pku.edu.cn>
2023-06-06 01:12:08 +0800

    Add iframe auxiliary message.

--- themes/pkmuon/source/js/pkmuon.js
+++ themes/pkmuon/source/js/pkmuon.js
@@ -1,17 +1,28 @@
 /*
- * Relocate PKMUON resource.
- *
+ * Provide PKMUON functionalities.
+ */
+
+"use strict";
+
+/* Hyperlink Relocation
+ * ------------------------------
  * Each element `e` matching `.pkmuon-relocation-src` is relocated
  * by adding `pkmuon_relocation_root` before `e.getAttribute("_src")`
  * to form `e.src`.
  */
 
-"use strict";
-
+// Hyperlink relocation: src.
 $(".pkmuon-relocation-src").each((i, e) => {
   e.src = pkmuon_relocation_root + e.getAttribute("_src");
 });
 
+// Hyperlink relocation: href.
 $(".pkmuon-relocation-href").each((i, e) => {
   e.href = pkmuon_relocation_root + e.getAttribute("_href");
+});
+
+// Message shown if iframe is not supported.
+$("iframe").each((i, e) => {
+  const msg = "<p>Preview is not supported on this browser.</p>";
+  e.innerHTML = e.innerHTML || msg;
 });
